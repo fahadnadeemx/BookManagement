89b038cf3797f6a6b2f2f2ba7f7f548a1a541d60
fahadnadeemx <85766937+fahadnadeemx@users.noreply.github.com>
2022-03-01 03:12:23 +0100

    ControllerTest CreateBook | Passed

--- src/test/java/com/book/bookmanagementt/controller/BookControllerTest.java
+++ src/test/java/com/book/bookmanagementt/controller/BookControllerTest.java
@@ -109,6 +109,8 @@ package com.book.bookmanagementt.controller;
 import com.book.bookmanagementt.BookmanagementtApplication;
 import com.book.bookmanagementt.entity.Book;
 import com.book.bookmanagementt.service.BookService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -116,6 +118,7 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -123,11 +126,14 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import java.util.ArrayList;
 import java.util.List;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 //@WebMvcTest(BookController.class)
@@ -137,6 +143,9 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @SpringBootTest(classes = BookmanagementtApplication.class)
 public class BookControllerTest {
     private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
 
     @Mock
     private BookService bookService;
@@ -157,6 +166,22 @@ public class BookControllerTest {
         bookList.add(new Book(3,"third", "first",30));
         Mockito.when(bookService.loadAllBooks()).thenReturn(bookList);
         String url = "/books";
-        mockMvc.perform(get(url)).andExpect(status().isOk());
+        MvcResult mvcResult = mockMvc.perform(get(url)).andExpect(status().isOk()).andReturn();
+
+        String actualJsonResponse = mvcResult.getResponse().getContentAsString();
+        System.out.println(actualJsonResponse);
+
+    }
+
+    @Test
+    public void testCreateBook() throws Exception{
+        Book newBook = new Book(1,"first", "first",10);
+        Book savedBook =new Book(1,"first", "first",10);
+
+        Mockito.when(bookService.saveBook(newBook)).thenReturn(savedBook);
+        String url = "/books/save";
+        mockMvc.perform(post(url).contentType("application/json")
+                        .content(objectMapper.writeValueAsString(newBook))).
+                andExpect(status().isOk());
     }
 }
