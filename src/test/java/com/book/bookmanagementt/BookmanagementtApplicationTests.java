56bc921366a4f11c7db2dd65087055a5e262eb92
fahadnadeemx <85766937+fahadnadeemx@users.noreply.github.com>
2022-03-11 22:01:56 +0100

    reload dependency of maven

--- src/test/java/com/book/bookmanagementt/BookmanagementtApplicationTests.java
+++ src/test/java/com/book/bookmanagementt/BookmanagementtApplicationTests.java
@@ -1,24 +1,24 @@
-//package com.book.bookmanagementt;
-//import com.book.bookmanagementt.controller.BookController;
-//import org.junit.jupiter.api.Test;
-//import org.junit.jupiter.api.extension.ExtendWith;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.boot.test.context.SpringBootTest;
-//import org.springframework.test.context.junit.jupiter.SpringExtension;
-//import static org.junit.jupiter.api.Assertions.assertNotNull;
-//
-//@ExtendWith(SpringExtension.class)
-//@SpringBootTest
-//class BookmanagementtTestIT {
-//
-//    @Autowired
-//    BookController bookController;
-//    @Test
-//    void contextLoads() {
-//        assertNotNull(bookController);
-//    }
-//
-//}
-//
-//
-//
+package com.book.bookmanagementt;
+import com.book.bookmanagementt.controller.BookController;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+class BookmanagementtTestIT {
+
+    @Autowired
+    BookController bookController;
+    @Test
+    void contextLoads() {
+        assertNotNull(bookController);
+    }
+
+}
+
+
+
