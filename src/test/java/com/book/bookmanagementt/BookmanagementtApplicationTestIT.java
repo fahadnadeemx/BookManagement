0bc2d2322084eb7f83d5c69bd4c4fb45a1f6df23
fahadnadeemx <85766937+fahadnadeemx@users.noreply.github.com>
2022-04-30 05:31:20 +0200

    checking sonar and coverall coveraage

---
+++ src/test/java/com/book/bookmanagementt/BookmanagementtApplicationTestIT.java
@@ -0,0 +1,23 @@
+package com.book.bookmanagementt;
+
+import com.book.bookmanagementt.controller.BookController;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+class BookmanagementtApplicationTestIT {
+
+    @Autowired
+    BookController bookController;
+    @Test
+    void contextLoads() {
+        assertNotNull(bookController);
+    }
+
+}
