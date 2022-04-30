3f2dbf182dbd432babe0a30dc94b4b3adbc0a4a0
fahadnadeemx <85766937+fahadnadeemx@users.noreply.github.com>
2022-04-30 05:54:37 +0200

    CHEKCING

---
+++ src/test/java/com/book/bookmanagementt/BookmanagementtApplicationTests.java
@@ -0,0 +1,14 @@
+package com.book.bookmanagementt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+@SpringBootTest
+class BookmanagementtApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
